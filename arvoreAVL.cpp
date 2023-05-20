1a6e421c82a69b66dd984bbf2e7918aa785b2f70
Kensa-Dudu <117110230+Kensa-Dudu@users.noreply.github.com>
2023-05-20 19:13:07 -0300

    Update arvoreAVL.cpp
    Função rotate right adicionada

--- arvoreAVL.cpp
+++ arvoreAVL.cpp
@@ -29,7 +29,18 @@ Node* newNode(int key){
 }
 //eduardo
 Node *rightRotate(Node *y){
+Node *x, *z;
+    x = y->left;
 
+    z = y->right;
+
+    x->right = y;
+
+    y->left = z; 
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+    return y;
 }
 //rafael
 Node *leftRotate(Node *x){
